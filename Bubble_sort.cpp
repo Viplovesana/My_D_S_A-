11dd4a6f7459ad4bd608cb3a64c2bc7d555ff6a4
Viplovesana <viplovesana90@gmail.com>
2025-05-11 02:34:19 +0530

    new commit

--- Bubble_sort.cpp
+++ Bubble_sort.cpp
@@ -1,14 +1,36 @@
 //..print an array
 
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+// int arr[5]={7,2,9,6,4};
+
+// for (int i=0;i<5;i++)
+// {
+//     cout<<arr[i]<<endl;
+// }
+
+// }
+
+
+//..update an array
+
 #include<iostream>
 using namespace std;
-
-int main(){
-int arr[5]={7,2,9,6,4};
-
-for (int i=0;i<5;i++)
-{
-    cout<<arr[i]<<endl;
+void showdata(int a[]){
+    for (int i=0;i<6;i++){
+        a[i]=a[i]+2;
+        cout<<a[i]<<endl;
+    }
 }
-
+int main(){
+    int arr[6]={7,3,5,8,6,9};
+    showdata(arr);
+    {
+        cout<<"after"<<endl;
+    }
+    for(int i=0;i<6;i++){
+        cout<<arr[i]<<endl;
+    }
 }

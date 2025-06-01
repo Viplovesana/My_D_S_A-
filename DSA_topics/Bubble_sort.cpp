ba84007cb2a3655de38893b92e849aa6bb36fcbc
Viplovesana <viplovesana90@gmail.com>
2025-06-01 08:53:25 +0530

    new commit

--- DSA_topics/Bubble_sort.cpp
+++ DSA_topics/Bubble_sort.cpp
@@ -227,40 +227,60 @@
 // }
 
 
+// #include<iostream>
+// using namespace std;
+// void bubble(int arr[],int s)
+// {
+//     int temp,c=0;
+//     bool d;
+//     for (int i=0;i<s;i++)
+//     {   
+//         d=false;   
+//         for (int j=0;j<s-i-1;j++)
+//         {
+//             c++;
+//             if(arr[j]>arr[j+1])
+//             {
+//                 temp=arr[j+1];
+//                 arr[j+1]=arr[j];
+//                 arr[j]=temp;
+//                 d=true;
+//             }        
+//         } 
+//         if (d==false){
+//             break;
+//         }
+//     }
+//     cout<<"counter hit "<<c<<endl;
+// }
+// int main()
+// {
+//     int arr[6]={1,2,3,4,5,6};
+//     bubble (arr,6);
+//     for(int i=0;i<6;i++)
+//     {
+//         cout<<arr[i]<<endl;
+//     }
+// }
+
+
 #include<iostream>
 using namespace std;
-void bubble(int arr[],int s)
-{
-    int temp,c=0;
-    bool d;
-    for (int i=0;i<s;i++)
-    {
-        for (int j=0;j<s-i-1;j++)
-        {
-            c++;
-            if(arr[j]>arr[j+1])
-            {
-                temp=arr[j+1];
-                arr[j+1]=arr[j];
-                arr[j]=temp;
-                d=true;
-            }        
-        } 
-        if (d==false){
-            break;
-        }
-    }
-    cout<<"counter hit "<<c<<endl;
-}
 int main()
 {
-    int arr[6]={1,2,3,4,5,6};
-    bubble (arr,6);
-    for(int i=0;i<6;i++)
+    int x;
+    cout<<"Enter the size of an array"<<endl;
+    cin>>x;
+    int arr[x];
+    
+    cout <<"Enter "<<x<<" value"<<endl;
+    for (int i=0;i<x;i++)
     {
-        cout<<arr[i]<<endl;
+        cin>>arr[i];
     }
 }
+
+
 
 
 

09144f915a43b7acc5dcbda4562ebdb2f88f784b
Viplovesana <viplovesana90@gmail.com>
2025-06-01 08:10:27 +0530

    new commit

--- DSA_topics/Bubble_sort.cpp
+++ DSA_topics/Bubble_sort.cpp
@@ -232,7 +232,7 @@ using namespace std;
 void bubble(int arr[],int s)
 {
     int temp,c=0;
-    
+    bool d;
     for (int i=0;i<s;i++)
     {
         for (int j=0;j<s-i-1;j++)
@@ -243,15 +243,18 @@ void bubble(int arr[],int s)
                 temp=arr[j+1];
                 arr[j+1]=arr[j];
                 arr[j]=temp;
-
+                d=true;
             }        
         } 
+        if (d==false){
+            break;
+        }
     }
     cout<<"counter hit "<<c<<endl;
 }
 int main()
 {
-    int arr[6]={3,5,2,7,9,2,};
+    int arr[6]={1,2,3,4,5,6};
     bubble (arr,6);
     for(int i=0;i<6;i++)
     {

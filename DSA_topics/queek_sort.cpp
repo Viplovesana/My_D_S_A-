8edc76d26e9670c3e12bd7b798e57d427556db2a
Viplovesana <viplovesana90@gmail.com>
2025-06-06 12:21:53 +0530

    new commit

--- DSA_topics/queek_sort.cpp
+++ DSA_topics/queek_sort.cpp
@@ -59,111 +59,117 @@
 
 ////..................................................................................
 
-// #include<iostream>
-// using namespace std;
-// int part(int arr[], int low ,int high){
-//     int pvt=arr[low];
-//     int i=low+1;
-//     int j=high;
-//     int swp;
-
-//     do{
-//       while(arr[i]<pvt){
-//         i++;
-//       }
-//    while(arr[j]>pvt){
-//         j--;
-//       }
-//       if(i<j){
-//         swp=arr[i];
-//         arr[i]=arr[j];
-//         arr[j]=swp;
-//       }
-//     }while(i<j);
-//      swp=arr[j];
-//      arr[j]=pvt;
-//      pvt=swp;
-//     return j;
-//     }
-
-// void Quicksort(int arr[], int low ,int high){
-//     int pivot;
-//     if(low<high){
-//         pivot=part(arr, low, high);
-//         Quicksort(arr, low , pivot-1);
-//         Quicksort(arr, pivot+1, high);                
-//     }
-
-// }
-
-// int main(){
-//     int s;
-//     cout<<"Enter the size of array :";
-//     cin>>s;
-//     int arr[s];
-//     for(int i=0;i<s;i++){
-//         cin>>arr[i];
-//     }
-//     Quicksort(arr, 0, s-1);
-//     for(int i=0;i<s;i++){
-//         cout<<arr[i]<<" ";
-//     }
-// }
-
-
-///.................................................................
-
 #include<iostream>
 using namespace std;
-int partfun(int arr[],int low ,int high)
-{
-    int pvt = arr[low];
-    int i = low+1;
-    int j =high;
+int part(int arr[], int low ,int high){
+    int pvt=arr[low];
+    int i=low+1;
+    int j=high;
     int swp;
-    
+
     do{
-        while(arr[i]<pvt){
-            i++;
-        }
-        while(arr[j]>pvt){
-            j--;
-        }
-        if(i<j){
-            swp=arr[j];
-            arr[j]=arr[i];
-            arr[i]=swp;
-        }
+      while(arr[i]<pvt){
+        i++;
+      }
+      while(arr[j]>pvt){
+        j--;
+      }
+      if(i<j){
+        swp=arr[j];
+        arr[j]=arr[i];
+        arr[i]=swp;
+      }
+    }while(i<j);
+     swp=arr[j];
+     arr[j]=arr[low];
+     arr[low]=swp;
+     return j;
     }
-    while(i<j);
-    swp=arr[j];
-    arr[j]=pvt;
-    pvt=swp;
-    return j;
-}
-void Quicksort(int arr[],int low,int high)
-{
+
+void Quicksort(int arr[], int low ,int high){
     int pivot;
     if(low<high){
-        pivot=partfun(arr,low,high);
-        Quicksort(arr,low,pivot-1);
-        Quicksort(arr,pivot+1,high);
+        pivot=part(arr, low, high);
+        Quicksort(arr, low , pivot-1);
+        Quicksort(arr, pivot+1, high);                
     }
+
 }
-int main()
-{
+
+int main(){
     int s;
-    cout<<"Enter the size of an array :"<<endl;
+    cout<<"Enter the size of array :";
     cin>>s;
-    int arr[s];
+    int arr[s]; 
     cout<<"Enter "<<s<<" values"<<endl;
     for(int i=0;i<s;i++){
         cin>>arr[i];
     }
-    Quicksort(arr,0,s-1);
+    Quicksort(arr, 0, s-1);
     cout<<"After sorted Array"<<endl;
-
     for(int i=0;i<s;i++){
-        cout<<arr[i]<<endl;     
-    }   
+        cout<<arr[i]<<endl;
+    }
 }
+
+
+///.................................................................
+
+// #include<iostream>
+// using namespace std;
+// int partfun(int arr[],int low ,int high)
+// {
+//     int pvt = arr[low];
+//     int i = low+1;
+//     int j =high;
+//     int swp;
+    
+//     do{
+//         while(arr[i]<pvt)
+//         {
+//             i++;
+//         }
+//         while(arr[j]>pvt)
+//         {
+//             j--;
+//         }
+//         if(i<j)
+//         {
+//             swp=arr[j];
+//             arr[j]=arr[i];
+//             arr[i]=swp;
+//         }
+//     }
+//     while(i<j);
+//     swp=arr[j];
+//     arr[j]=arr[low];
+//     arr[low]=swp;
+//     return j;
+// }
+// void Quicksort(int arr[],int low,int high)
+// {
+//     int pivot;
+//     if(low<high)
+//     {
+//         pivot=partfun(arr,low,high);
+//         Quicksort(arr,low,pivot-1);
+//         Quicksort(arr,pivot+1,high);
+//     }
+// }
+// int main()
+// {
+//     int s;
+//     cout<<"Enter the size of an array :"<<endl;
+//     cin>>s;
+//     int arr[s];
+//     cout<<"Enter "<<s<<" values"<<endl;
+//     for(int i=0;i<s;i++){
+//         cin>>arr[i];
+//     }
+//     Quicksort(arr,0,s-1);
+//     cout<<"After sorted Array"<<endl;
+
+//     for(int i=0;i<s;i++){
+//         cout<<arr[i]<<endl;     
+//     }   
+// }

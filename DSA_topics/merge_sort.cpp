7889d4e6f0fd100794ceac8090f505df9fb0666a
Viplovesana <viplovesana90@gmail.com>
2025-06-10 23:07:00 +0530

    new commit

--- DSA_topics/merge_sort.cpp
+++ DSA_topics/merge_sort.cpp
@@ -1,63 +1,76 @@
 
+// #include<iostream>
+// using namespace std;
+// void merges(int arr[],int low,int mid,int high){
+//     int b[high+1];
+//     int i=low;
+//     int j=mid+1;
+//     int k=low;
+//     while(i<=mid && j<=high){
+//         if(arr[i]<arr[j])
+//    {
+// b[k]=arr[i];
+// i++;
+// } 
+// else
+// {
+//     b[k]=arr[j];
+//     j++;
+// }
+// k++;
+//     }
+//     while(i<=mid)
+// {
+//     b[k]=arr[i];
+//     i++;
+//     k++;
+// }
+// while(j<=high)
+// {
+//     b[k]=arr[j];
+//     j++;
+//     k++;
+// }
+// for(int i=low;i<=high;i++)
+// {
+// arr[i]=b[i];
+// }
+// }
+// void mergesort(int arr[],int low, int high){
+//     int mid;
+//     if(low<high){
+//         mid=(low+high)/2;
+//         mergesort(arr,low,mid);
+//         mergesort(arr,mid+1,high);
+//         merges(arr,low,mid,high);
+//     }
+// }
+// int main(){
+//     int arr[]={6,8,1,2,4,3,5,9};
+//     int s=sizeof(arr)/sizeof(arr[0]);
+//     for(int i=0;i<s;i++){
+//         cout<<arr[i]<<endl;
+//     }
+//     mergesort(arr,0,s-1);
+//     cout<<"After mergesort"<<endl;
+//     for (int i=0;i<s;i++){
+//         cout<<arr[i]<<endl;
+//     }
+// }
+
+//...........................................................................
+
 #include<iostream>
 using namespace std;
-void merges(int arr[],int low,int mid,int high)
-{
-    int b[high+1];
-    int i=low;
-    int j=mid+1;
-    int k=low;
-    while(i<=mid && j<=high)
-    {
-        if(arr[i]<arr[j])
-   {
-b[k]=arr[i];
-i++;
-}
-else
-{
-    b[k]=arr[j];
-    j++;
-}
-k++;
-    }
-    while(i<=mid)
-{
-    b[k]=arr[i];
-    i++;
-    k++;
-}
-while(j<=high)
-{
-    b[k]=arr[j];
-    j++;
-    k++;
-}
-for(int i=low;i<=high;i++)
-{
-arr[i]=b[i];
-}
-}
-
-void mergesort(int arr[],int low, int high)
-{
-    int mid;
-    if(low<high){
-        mid=(low+high)/2;
-        mergesort(arr,low,mid);
-        mergesort(arr,mid+1,high);
-        merges(arr,low,mid,high);
-    }
-}
-int main()
-{
-    int arr[]={6,8,1,2,4,3,5,9};
+int main (){
+    int arr[]={10,3,6,9,3,6,};
     int s=sizeof(arr)/sizeof(arr[0]);
-    for(int i=0;i<s;i++){
+    for(int i=0;i<s;i++)l;
+    {
         cout<<arr[i]<<endl;
     }
     mergesort(arr,0,s-1);
-    cout<<"After mergesort"<<endl;
+    cout<<"after mergesort"<<endl;
     for (int i=0;i<s;i++){
         cout<<arr[i]<<endl;
     }

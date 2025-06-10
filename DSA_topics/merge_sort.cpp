f82556c78ab280d9accc91b085607f675e898f15
Viplovesana <viplovesana90@gmail.com>
2025-06-10 23:14:07 +0530

    new commit

--- DSA_topics/merge_sort.cpp
+++ DSA_topics/merge_sort.cpp
@@ -62,6 +62,36 @@
 
 #include<iostream>
 using namespace std;
+void merges(int arr[],int low,int mid,int high){
+    int b[high+1];
+    int i=low;
+    int j=mid+1;
+    int k=low;
+    while(i<=mid && j<=high){
+        if(arr[i]<arr[j]){
+            b[k]=arr[i];
+            i++;
+        }
+        else{
+            b[k]=arr[j];
+            j++;
+        }
+        k++
+    }
+    while(i<=mid){
+        b[k]=arr[i];
+            i++;
+            k++;
+    }
+    while(j<=high){
+        b[k]=arr[j];
+            j++;
+            k++;
+    }
+    for (int i=low;i<high;i++){
+        arr[i]=b[i];
+    }
+}
 void mergesort(int arr[],int low ,int high){
     int mid;
     if(low<high){

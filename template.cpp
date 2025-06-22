8fb820f5118c6ca276343803cd44351d146c6b3c
Viplovesana <viplovesana90@gmail.com>
2025-06-22 07:27:22 +0530

    new commit

--- template.cpp
+++ template.cpp
@@ -90,16 +90,15 @@
 using namespace std;
 template <class obj1,class obj2>
 
-class Template{
-    public:
+class Job{
     obj1 v1 ;
     obj2 v2 ;
-    Template(obj val1,obj val2){
+public:Job(obj1 val1,obj1 val2){
         v1 = val1;
         v2 = val2;
         cout<<"value ="<<v1+v2;
     }
 };
 int main(){
-    Template<float,float>j(52,42.2);
+Job<float,float>j(52,42.2);
 }
